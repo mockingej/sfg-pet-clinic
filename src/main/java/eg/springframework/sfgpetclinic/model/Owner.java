03cffc1d3733b72421d1cc08e20e7a596e0eb638
mockingej <erwinjay.grande@gmail.com>
2019-01-15 22:13:30 +0800

    Adding POJOs. Closes #2.

---
+++ src/main/java/eg/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package eg.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/eg/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package eg.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
