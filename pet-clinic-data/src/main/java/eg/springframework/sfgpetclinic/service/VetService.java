815bb7211226808402d03fc0062421c33607c03b
mockingej <erwinjay.grande@gmail.com>
2019-01-15 23:27:19 +0800

    Implement interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/eg/springframework/sfgpetclinic/service/VetService.java
@@ -0,0 +1,13 @@
+package eg.springframework.sfgpetclinic.service;
+
+import eg.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
