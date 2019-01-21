0d40876863bd16b2f23514242ed7ec2ac5701c68
mockingej <erwinjay.grande@gmail.com>
2019-01-21 23:05:16 +0800

    Implemented map based services. Closes #6

---
+++ pet-clinic-data/src/main/java/eg/springframework/sfgpetclinic/service/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package eg.springframework.sfgpetclinic.service.map;
+
+import eg.springframework.sfgpetclinic.model.Vet;
+import eg.springframework.sfgpetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
